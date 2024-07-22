523fb2ce2529f443fcb9424beca6f0001ce8aa96
José Teles <josetelesmaciel@gmail.com>
2024-07-22 20:36:14 -0300

    chore: Add fetchAllPostSlugs function for retrieving all post slugs

--- src/lib/services/postServices.ts
+++ src/lib/services/postServices.ts
@@ -4,6 +4,15 @@ import { adaptPost } from "../adapters/postAdapter";
 
 const apiUrl = import.meta.env.PUBLIC_WP_JSON_API_URL;
 
+interface WPResponse<T> {
+  json: () => Promise<T[]>;
+  headers: {
+    get: (arg0: string) => string;
+  };
+}
+
+interface WPPostsResponse extends WPResponse<IncomingPost> {}
+
 export const fetchPostBySlug = async (slug: string): Promise<Post | null> => {
   const response = await fetch(`${apiUrl}/posts?slug=${slug}`);
 
@@ -22,4 +31,29 @@ export const fetchPostBySlug = async (slug: string): Promise<Post | null> => {
   }
 
   return adaptPost(data[0]);
+};
+
+export const fetchAllPostSlugs = async (): Promise<
+  Pick<IncomingPost, "slug">[]
+> => {
+  const slugs: Pick<IncomingPost, "slug">[] = [];
+  let page = 1;
+  let totalPages = 1;
+
+  while (page <= totalPages) {
+    const response = (await fetch(
+      `${apiUrl}/posts?per_page=100&_fields=slug&page=${page}`
+    )) as WPPostsResponse;
+    const posts = await response.json();
+
+    posts.forEach((post) => slugs.push({ slug: post.slug }));
+
+    if (page === 1) {
+      totalPages = parseInt(response.headers.get("X-WP-TotalPages"), 10);
+    }
+
+    page++;
+  }
+
+  return slugs;
 };
